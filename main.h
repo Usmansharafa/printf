e40f7e78249656765d1012c037fa8a3554194066
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-15 09:48:10 -0700

    Added starter files

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int print_int(char *s);
+int print_char(char c);
+int print_str(char *s);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
