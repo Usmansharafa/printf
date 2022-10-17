1281c7cecd7ff81c5e24de654acebe78bc42bf1c
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-17 01:45:03 +0100

    Fixed betty errors

--- base_converters.c
+++ base_converters.c
@@ -7,29 +7,29 @@
  */
 int print_binary(va_list arg)
 {
-    unsigned int num = va_arg(arg, unsigned int);
-    char *digit;
-    int i, len;
+	unsigned int num = va_arg(arg, unsigned int);
+	char *digit;
+	int i, len;
 
-    if (num == 0)
-        return (_putchar('0'));
-    if (num < 1)
-        return (-1);
-    len = base_len(num, 2);
-    digit = malloc(sizeof(char) * len + 1);
-    if (digit == NULL)
-        return (-1);
+	if (num == 0)
+		return (_putchar('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 2);
+	digit = malloc(sizeof(char) * len + 1);
+	if (digit == NULL)
+		return (-1);
 
-    for (i = 0; num > 0; i++)
-    {
-        if (num % 2 == 0)
-            digit[i] = '0';
-        else
-            digit[i] = '1';
-        num = num / 2;
-    }
-    digit[i] = '\0';
-    print_rev(digit);
-    free(digit);
-    return (len);
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			digit[i] = '0';
+		else
+			digit[i] = '1';
+		num = num / 2;
+	}
+	digit[i] = '\0';
+	print_rev(digit);
+	free(digit);
+	return (len);
 }
